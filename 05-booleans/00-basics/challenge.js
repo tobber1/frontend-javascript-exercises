3a399f0e3bd0b7e4228d3b94e3c2b78a6cd8ccda
Todd Caschette <laugh_aloud80026@yahoo.com>
2014-12-01 21:43:37 +0000

    3 of 19 still fail mocha test

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,20 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  return stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return one !== two
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  return (lower < middle) && (middle < upper); 
 };
 
 module.exports.outsideRanges = function(number) {
-
+  return ((10 > number) || (number > 20)) || ((42 >= number) || (number > 75)) || ((1 >= number) || (number >=6));
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  return (name) && (price >= 1);
 };
+
