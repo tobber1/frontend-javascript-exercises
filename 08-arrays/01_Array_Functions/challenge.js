4e16271bf34f9f189e2ecc8f74489dddbaad3c08
Todd Caschette <laugh_aloud80026@yahoo.com>
2014-12-05 23:56:02 +0000

    All pass array functions tests

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,51 @@
-module.exports.reversePlusOne = undefined;
+/*  Create a function named `reversePlusOne`. This function should:
+    - Take one argument, an array of at least two numbers.
+    - This function should return:
+        - the array *reversed* with a `1` added at the front
 
-module.exports.plusesEverywhere = undefined;
+    For example:
+    
+    ```javascript
+    reversePlusOne([1,2]); // returns [1,2,1]
+    reversePlusOne([5,4,3,2]); // returns [1,2,3,4,5]
+    ```
+*/
+module.exports.reversePlusOne = function (arr){
+  var newArr = arr.reverse();
+  
+  arr.unshift(1);
+  
+  return arr;
 
-module.exports.arrayQuantityPlusOne = undefined;
+}
+/* reate a function named `plusesEverywhere`. This function should:
+	- Take one argument, an array of at least two numbers.
+	- This function should return:
+		- a String made of all the values in the array separated by `+`
+		
+	For example:
+	
+	```javascript
+	plusesEverywhere([1,2,3]); // returns "1+2+3"
+	plusesEverywhere([18,24]); // returns "18+24"
+	```*/
+
+module.exports.plusesEverywhere = function (arr){
+  return arr.join('+');
+}
+
+/* Create a function named `arrayQuantityPlusOne`. This function should:
+	- Take one argument, an array of numbers.
+	- This function should return:
+		- one greater than the number of items in the array
+
+	For example: 
+	
+	```javascript
+	arrayQuantityPlusOne([0,0,1,0,2,1]); // returns 7
+	arrayQuantityPlusOne([42]); // returns 2
+	*/
+
+module.exports.arrayQuantityPlusOne = function (arr) {
+  return arr.length + 1;
+}
