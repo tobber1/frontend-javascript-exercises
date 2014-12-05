7683414f0eaf5a9c80d5c5d5afbb34d2f699ecec
Todd Caschette <laugh_aloud80026@yahoo.com>
2014-12-05 23:53:08 +0000

    all pass

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -2,6 +2,7 @@ module.exports.newArray = function(a, b, c, d) {
   return [a,b,c,d];
 }
 
+
 module.exports.firstAndLast = function(arr) {
   var lastElementIndex = arr.length - 1;
   return [arr[0], arr[lastElementIndex]];
